fc2532bcfe958acb46889d777716fbf7b9ab9ebe
NISH1001 <nishanpantha@gmail.com>
2019-06-19 09:44:39 +0545

    Add generation script.

--- coffee-with-nish/episode-08-remembrance/generator.py
+++ coffee-with-nish/episode-08-remembrance/generator.py
@@ -11,21 +11,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('dark_background')
 
-from panim.spiral import SpiralAnimator, SpiralAnimator2
+from panim.generative import (
+    GenerativeArt
+)
 from panim.transformers import ZoomTransformer
 
 
 def main():
-    n = 25
-    animator = SpiralAnimator2(
-        interval=50,
-        scale=0.4,
-        nlimit=n,
-        npoints=50,
-        rot_factor=0.001
+    animator = GenerativeArt(
+        width=640,
+        height=320
     )
-    animator.animate(9000)
-    animator.save("out/spiral2.mp4")
+    animator.animate(2350)
+    animator.save("out/generative2.mp4", fps=7)
 
 if __name__ == "__main__":
     main()
