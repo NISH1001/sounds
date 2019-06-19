090bf9ece21dcdf7139a45c0583bb0573723fd75
NISH1001 <nishanpantha@gmail.com>
2019-06-19 10:13:31 +0545

    Add generation script for episode 8

--- coffee-with-nish/episode-08-remembrance/generator.py
+++ coffee-with-nish/episode-08-remembrance/generator.py
@@ -16,6 +16,24 @@ from panim.generative import (
 )
 from panim.transformers import ZoomTransformer
 
+class GenerativeArt(AbstractImageAnimator):
+    def __init__(self, **args):
+        super().__init__(**args)
+        self.r, self.a = meshgrid_polar(self.image_size)
+        self.lr = np.log(1 + self.r)
+        self.factor = -50
+
+    def update(self, i):
+        if -5 < self.factor < 5:
+            self.factor += 0.01
+        else:
+            self.factor += 0.2
+        im = np.sin(self.a * self.factor + np.sin(self.lr * 4) + self.lr*2)
+        im = np.fmod((1 + im + self.lr), 1)
+        im = get_image_array(im)
+        im = self.ax.imshow(im, animated=True, cmap='gray')
+        self.images.append([im])
+
 
 def main():
     animator = GenerativeArt(
